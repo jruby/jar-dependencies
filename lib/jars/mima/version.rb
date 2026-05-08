4e35aa1a3c42496bfd16d94417025b7388fb0e58
Charles Oliver Nutter <headius@headius.com>
2026-05-08 17:49:31 -0500

    Clean up jar hash building

--- lib/jars/mima/version.rb
+++ lib/jars/mima/version.rb
@@ -8,28 +8,35 @@ module Jars
     MAVEN_CENTRAL = 'https://repo.maven.apache.org/maven2'
     MIMA_DIR = 'lib/jars/mima'
 
-    # URL and SHA-1 checksum (as published on Maven Central) for each jar
-    JARS = {
-      "slf4j-api-#{SLF4J_VERSION}.jar" => {
-        url: "#{MAVEN_CENTRAL}/org/slf4j/slf4j-api/#{SLF4J_VERSION}/slf4j-api-#{SLF4J_VERSION}.jar",
-        sha1: 'd9e58ac9c7779ba3bf8142aff6c830617a7fe60f'
-      },
-      "slf4j-simple-#{SLF4J_VERSION}.jar" => {
-        url: "#{MAVEN_CENTRAL}/org/slf4j/slf4j-simple/#{SLF4J_VERSION}/slf4j-simple-#{SLF4J_VERSION}.jar",
-        sha1: '9872a3fd794ffe7b18d17747926a64d61526ca96'
-      },
-      "jcl-over-slf4j-#{SLF4J_VERSION}.jar" => {
-        url: "#{MAVEN_CENTRAL}/org/slf4j/jcl-over-slf4j/#{SLF4J_VERSION}/jcl-over-slf4j-#{SLF4J_VERSION}.jar",
-        sha1: '76ea503eb688f06556a9ba69995d7eab63e34531'
-      },
-      "context-#{MIMA_VERSION}.jar" => {
-        url: "#{MAVEN_CENTRAL}/eu/maveniverse/maven/mima/context/#{MIMA_VERSION}/context-#{MIMA_VERSION}.jar",
-        sha1: '72aa4d9ccef7a329f473e43752ec863c5194c72c'
-      },
-      "standalone-static-uber-#{MIMA_VERSION}.jar" => {
-        url: "#{MAVEN_CENTRAL}/eu/maveniverse/maven/mima/runtime/standalone-static-uber/#{MIMA_VERSION}/standalone-static-uber-#{MIMA_VERSION}.jar",
-        sha1: '43666099e6eb31610f9d3b146811479dd3e4aef1'
-      }
-    }.freeze
+    # GAV and SHA-1 checksum (as published on Maven Central) for each jar
+    jars = %w[
+      org.slf4j:slf4j-api:2.0.17
+      d9e58ac9c7779ba3bf8142aff6c830617a7fe60f
+
+      org.slf4j:slf4j-simple:2.0.17
+      9872a3fd794ffe7b18d17747926a64d61526ca96
+
+      org.slf4j:jcl-over-slf4j:2.0.17
+      76ea503eb688f06556a9ba69995d7eab63e34531
+
+      eu.maveniverse.maven.mima:context:2.4.42
+      72aa4d9ccef7a329f473e43752ec863c5194c72c
+
+      eu.maveniverse.maven.mima.runtime:standalone-static-uber:2.4.42
+      43666099e6eb31610f9d3b146811479dd3e4aef1
+    ]
+
+    JARS = jars.each_slice(2).to_h do |gav, sha1|
+      group, artifact, version = gav.split(':')
+      group = group.tr('.', '/')
+      jar_file = "#{artifact}-#{version}.jar"
+      [
+        jar_file,
+        {
+          url: "#{MAVEN_CENTRAL}/#{group}/#{artifact}/#{version}/#{jar_file}",
+          sha1: sha1
+        }
+      ]
+    end
   end
 end
