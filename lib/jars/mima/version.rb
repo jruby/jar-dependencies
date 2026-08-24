f8e95b65ab538827221fe83259feececffb92db9
Charles Oliver Nutter <headius@headius.com>
2026-08-23 22:09:11 -0500

    Update mima and slf4j
    * mima context and standalone to 2.4.48
    * slf4j api, simple, and jcl to 2.0.18

--- lib/jars/mima/version.rb
+++ lib/jars/mima/version.rb
@@ -2,28 +2,28 @@
 
 module Jars
   module Mima
-    MIMA_VERSION = '2.4.42'
-    SLF4J_VERSION = '2.0.17'
+    MIMA_VERSION = '2.4.48'
+    SLF4J_VERSION = '2.0.18'
 
     MAVEN_CENTRAL = 'https://repo.maven.apache.org/maven2'
     MIMA_DIR = 'lib/jars/mima'
 
     # GAV and SHA-1 checksum (as published on Maven Central) for each jar
     jars = %w[
-      org.slf4j:slf4j-api:2.0.17
-      d9e58ac9c7779ba3bf8142aff6c830617a7fe60f
+      org.slf4j:slf4j-api:2.0.18
+      78a9e7a37cd6360e0b818e86341b24123d28d4df
 
-      org.slf4j:slf4j-simple:2.0.17
-      9872a3fd794ffe7b18d17747926a64d61526ca96
+      org.slf4j:slf4j-simple:2.0.18
+      503354e24cb3a2c61f808a83934b76c2dc4ec4d8
 
-      org.slf4j:jcl-over-slf4j:2.0.17
-      76ea503eb688f06556a9ba69995d7eab63e34531
+      org.slf4j:jcl-over-slf4j:2.0.18
+      fb36c10c70e27bb427bb3cec86ac30bb83f667c7
 
-      eu.maveniverse.maven.mima:context:2.4.42
-      72aa4d9ccef7a329f473e43752ec863c5194c72c
+      eu.maveniverse.maven.mima:context:2.4.48
+      e792b92b226c2bdc829030c19f1239553dc1e382
 
-      eu.maveniverse.maven.mima.runtime:standalone-static-uber:2.4.42
-      43666099e6eb31610f9d3b146811479dd3e4aef1
+      eu.maveniverse.maven.mima.runtime:standalone-static-uber:2.4.48
+      b2bb20acce196213d1cbd39cbe1cdb76d8a6b67b
     ]
 
     JARS = jars.each_slice(2).to_h do |gav, sha1|
